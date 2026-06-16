28f2f4efc0e177c599775eaac120d67cc04da98a
Rupesh Jumade <rupeshjumde9@gmail.com>
2026-06-16 08:51:53 +0000

    Create first flask service

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from pathlib import Path
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return {
+        "message": "Platform Demo App"
+    }
+
+
+@app.route("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
+
+
+@app.route("/version")
+def version():
+    version_file = Path("version.txt")
+
+    return {
+        "version": version_file.read_text().strip()
+    }
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
